5cf9f050b1e5d21c8b375253ac80ecacdc962d76
Weston Carlson <wez470@gmail.com>
2019-03-30 15:23:36 -0600

    Turn server into initial echo server.

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
+//Server the application server
+type server struct{}
+
+func (s *server) echoBody() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		buf, _ := ioutil.ReadAll(r.Body)
+		w.Write(buf)
+	}
+}
+
+func (s *server) echo() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.Write(w)
+	}
+}
+
 func main() {
+	s := server{}
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.RedirectSlashes)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
+	r.HandleFunc("/echo", s.echoBody())
+	r.HandleFunc("/echoAll", s.echo())
 	http.ListenAndServe(":8080", r)
 }
