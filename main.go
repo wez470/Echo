3393be26989ca194b825a074f7ae3c1e8e4bb974
Weston Carlson <wez470@gmail.com>
2019-03-30 21:50:43 -0600

    Add documentation generation.

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/docgen"
 )
 
 const port = "8080"
+
+var generateDocs = flag.Bool("docs", false, "Generate server route documentation")
 
 //Server the application server
 type server struct{}
@@ -35,12 +40,24 @@ func (s *server) echo() http.HandlerFunc {
 	}
 }
 
+func conditionallyPrintDocs(r *chi.Mux) {
+	if *generateDocs {
+		fmt.Println(docgen.MarkdownRoutesDoc(r, docgen.MarkdownOpts{
+			ProjectPath: "github.com/wez470/Echo",
+			Intro:       "Echo server generated docs.",
+		}))
+		return
+	}
+}
+
 func main() {
+	flag.Parse()
 	s := server{}
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.RedirectSlashes)
 	r.HandleFunc("/echoBody", s.echoBody())
 	r.HandleFunc("/echo", s.echo())
+	conditionallyPrintDocs(r)
 	http.ListenAndServe(":"+port, r)
 }
